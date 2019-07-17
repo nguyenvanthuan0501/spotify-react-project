12b8c568ddb62c1c4a54c909e43ff36814c2a06d
nguyenvanthuan0501 <thuan.nguyenvan0501.work>
2019-07-17 16:10:13 +0700

    Update debugger

--- dist/callback/main.js
+++ dist/callback/main.js
@@ -1,20 +1,22 @@
-const urlParams = new URLSearchParams(location.hash.slice(1))
+const QueryString = require('query-string')
 
-if (urlParams.get("access_token")) {
+const queryString = QueryString.parse(location.hash)
+
+if (queryString.error) {
   // We should get below
   // error
   // state
-  console.log('Spotify access has been denied' + urlParams.get("erorr"))
+  console.log('Spotify access has been denied' + queryString.error)
 } else {
   // We should get below
   //   access_token
   //   token_type
   //   expires_in
   //   state
-  if (opener && opener.spotifyApi) opener.spotifyApi.setAccessToken(urlParams.get("access_token"));
+  if (opener && opener.spotifyApi) opener.spotifyApi.setAccessToken(queryString.access_token);
   if (opener && opener.onAuth) opener.onAuth()
   // save token to local storage
-  localStorage.setItem('token', urlParams.get("access_token"))
+  localStorage.setItem('token', queryString.access_token)
 }
 
 window.close()
