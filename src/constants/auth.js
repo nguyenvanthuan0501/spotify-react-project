dddd42024f50af54cd5f5e8a0b6dfffd9776a32a
nguyenvanthuan0501 <thuan.nguyenvan0501.work>
2019-07-19 10:59:05 +0700

    Update uri

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,5 +1,5 @@
 export const CLIENT_ID = '71d3f41f6bfe497e8033de5677ca0058';
-export const REDIRECT_URI= `${window.location.protocol}//${window.location.host}/callback`;
+export const REDIRECT_URI= `https://nguyenvanthuan0501.github.io/spotify-react-project//callback`;
 
 export const SCOPES = [
   'user-read-private',
